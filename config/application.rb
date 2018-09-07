59020d3950b736b39e59ae7765136eaab9fac838
Vehabovic Nermin <nermin.vehabovic90@gmail.com>
2018-09-07 20:32:13 +0200

    Added: Translation files Added: Shared header Added: Default layout

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,11 @@ module ClickDonations
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
+    # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
+    # config.i18n.default_locale = :de
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.enforce_available_locales = true
   end
 end
