77cb95cf713e5fbae9350cd2acbd19c862300298
Nermin Vehabovic <nermin.vehabovic90@gmail.com>
2019-12-05 19:44:56 +0100

    Modified home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
-  devise_for :users
-end
-Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :fruits, only: [:index, :create, :destroy, :update]
     end
   end
-end
 
+  devise_for :users
+end
