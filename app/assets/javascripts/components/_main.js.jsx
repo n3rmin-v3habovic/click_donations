607f6f19bf709eb0326ba14337afc70d23358c4b
Ahmed Pirić <ahmedpiric997@gmail.com>
2019-12-10 19:19:19 +0100

    Initial react part

--- app/assets/javascripts/components/_main.js.jsx
+++ app/assets/javascripts/components/_main.js.jsx
@@ -3,7 +3,7 @@ const Main = (props) => {
     return(
         <div>
             <h1>Fruits are great!</h1>
-            <AllFruits />
+            <Body />
         </div>
     )
 }
--- app/assets/javascripts/components/_new_item.js.jsx
+++ app/assets/javascripts/components/_new_item.js.jsx
@@ -2,9 +2,14 @@ const NewFruit = (props) => {
     let formFields = {}
 
     return(
-        <form>
-            <input ref={input => formFields.name = input} placeholder='Enter the name of the item'/>
-            <input ref={input => formFields.description = input} placeholder='Enter a description' />
+        <form onSubmit={ (e) => {
+            props.handleFormSubmit(formFields.name.value,
+                formFields.description.value); e.target.reset();}
+        }>
+            <input ref={input => formFields.name = input}
+                   placeholder='Enter the name of the item'/>
+            <input ref={input => formFields.description = input}
+                   placeholder='Enter a description' />
             <button>Submit</button>
         </form>
     )
