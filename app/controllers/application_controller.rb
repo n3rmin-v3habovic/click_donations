d9ebcaea14f7fe0e33107890b3c60c5d28e178d9
Ahmed Pirić <ahmedpiric997@gmail.com>
2019-12-17 14:26:45 +0100

    Added after sign in path and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,7 @@
 class ApplicationController < ActionController::Base
   protected
+
   def after_sign_in_path_for(resource)
-    root_path
+    dashboards_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   root to: 'home#index'
 
+  get 'dashboards', to: 'dashboards#index'
+
   devise_for :users
 end
