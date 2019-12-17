509b5b1cb1926fc86929a1503f1b59189395fac1
Ahmed Pirić <ahmedpiric997@gmail.com>
2019-12-17 15:16:15 +0100

    Revert "Added after sign in path and view"
    This reverts commit d9ebcaea14f7fe0e33107890b3c60c5d28e178d9.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,6 @@
 class ApplicationController < ActionController::Base
   protected
-
   def after_sign_in_path_for(resource)
-    dashboards_path
+    root_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,5 @@ Rails.application.routes.draw do
   get 'hello_world', to: 'hello_world#index'
   root to: 'home#index'
 
-  get 'dashboards', to: 'dashboards#index'
-
   devise_for :users
 end
