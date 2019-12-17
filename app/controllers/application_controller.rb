9b009153f49b2d23f2fee8368645558471f15fff
Ahmed Pirić <ahmedpiric997@gmail.com>
2019-12-17 12:35:38 +0100

    Revert "Modified home controller"
    This reverts commit 77cb95cf713e5fbae9350cd2acbd19c862300298.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,9 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :null_session
-
   protected
   def after_sign_in_path_for(resource)
     root_path
   end
+end
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :null_session
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
+  devise_for :users
+end
+Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :fruits, only: [:index, :create, :destroy, :update]
     end
   end
-
-  devise_for :users
 end
+
